a100480c4ce96f6b1c68ccd6a6027d3cd4ab173f
gangbo.dai <daigangbo@gmail.com>
2020-09-29 14:07:54 +0800

    php7.2-nginx

---
+++ docker/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script is run within the php containers on start
+# docker-run.sh is available for legacy setups
+
+# Fail on any error
+set -o errexit
+
+touch /var/log/app_cron.log
+crontab /etc/cron.d/app.cron && cron && crontab -l
+
+if [ -x "usermod" ] ; then
+    usermod -u ${PHP_USER_ID} www
+fi
+
+
+# Run the command sent as command line arguments
+php-fpm
