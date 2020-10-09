ce576a6207c2eda2ae9a5298d8475f22966a32a5
gangbo.dai <daigangbo@gmail.com>
2020-10-09 13:47:07 +0800

    docker

--- docker/run.sh
+++ docker/run.sh
@@ -9,10 +9,14 @@ set -o errexit
 touch /var/log/app_cron.log
 crontab /etc/cron.d/app.cron && cron && crontab -l
 
-if [ -x "usermod" ] ; then
-    usermod -u ${PHP_USER_ID} www
-fi
+#if [ -x "usermod" ] ; then
+#    usermod -u ${PHP_USER_ID} www
+#fi
+
+mkdir -p /data/logs/xyf-app-api/app
+chown -R www-data:www-data /data/logs
 
 
+nginx
 # Run the command sent as command line arguments
 php-fpm
